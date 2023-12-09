78503f09aaabb73158cb16b993ce6d796ae50aa5
vens8 <vensr.maddula@gmail.com>
2023-12-09 15:08:37 +0530

    prod

--- django_backend/django_backend/production.py
+++ django_backend/django_backend/production.py
@@ -2,11 +2,14 @@ from .settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['54.83.17.157', 'test-website.taleofhumankind.com', '127.0.0.1', 'localhost', '127.0.0.1:8000']
-STATIC_URL = "/api/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/api/djangostatic/'
+STATIC_ROOT = '/home/ubuntu/static'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS=[(os.path.join(BASE_DIR,'static'))]
 
 MEDIA_URL = '/api/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/ubuntu/media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Session
 SESSION_COOKIE_SECURE = False  # True in production if using HTTPS
